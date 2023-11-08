b91a0cf2f22667eacf30cd90e077f9bf86b4a861
TonyStr <hello@tonystr.net>
2023-11-08 14:58:17 +0100

    blobs and animations

--- src/pages/home/HomeRoute.jsx
+++ src/pages/home/HomeRoute.jsx
@@ -1,15 +1,49 @@
+import { useEffect } from 'react';
 import styles from './HomeRoute.module.scss';
+import './HomeRoute.scss';
 import IconPlanetDash from '@/assets/planet-dash.svg?react';
+import { useRef } from 'react';
 
 export default function HomeRoute() {
+	const cursorBlob = useRef(null);
+
+	useEffect(() => {
+		if (cursorBlob.current === null) return;
+		document.body.addEventListener('mousemove', (e) => {
+			cursorBlob.current.animate({
+				left: `${e.clientX - 400}px`,
+				top:  `${e.clientY - 400}px`,
+			}, { duration: 7000, fill: 'forwards' });
+		});
+	}, [cursorBlob]);
+
     return (
-        <div className={styles.home}>
-			<div className={styles.planetContainer}>
-				<h1 className={styles.mainHeading}>
-					Tony Strømsnæs
+        <div className="home">
+			<div className="planet-container">
+				<h1 className="main-heading">
+					<span>T</span>
+					<span>o</span>
+					<span>n</span>
+					<span>y</span>
+					&nbsp;
+					<span>S</span>
+					<span>t</span>
+					<span>r</span>
+					<span>ø</span>
+					<span>m</span>
+					<span>s</span>
+					<span>n</span>
+					<span>æ</span>
+					<span>s</span>
 				</h1>
 				<IconPlanetDash className={styles.planet} />
 			</div>
+			<div
+				className="cursor-blob"
+				data-loaded={cursorBlob.current !== null}
+				ref={cursorBlob}
+			/>
+			<div className="blur-bg" />
         </div>
     );
 }
