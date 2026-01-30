4a854e7b189d03e82d3b175751f4713eb2bcc69b
TonyStr <hello@tonystr.net>
2026-01-30 12:09:37 +0100

    fix: swr->isr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
 	$production: {
 		routeRules: {
 			'/blog': { prerender: true, isr: true },
-			'/blog/**': { prerender: true, swr: 900 },
+			'/blog/**': { prerender: true, isr: true },
 		}
 	},
 	$development: {
