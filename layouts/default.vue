61b9e4717d8e0f52742409dd13d7f5d2f289df3a
TonyStr <hello@tonystr.net>
2026-03-03 13:57:20 +0100

    feat: vercel speed insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
 </script>
 
 <template>
 	<div class="page-wrapper">
 		<Analytics />
+		<SpeedInsights />
 		<NuxtLoadingIndicator />
 		<NuxtPage />
 	</div>
